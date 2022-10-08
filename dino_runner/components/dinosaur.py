c6cd21c9e443fa35061b26f911848d63aad66816
Brisa1515 <brisagilrivera@gmail.com>
2022-10-08 11:20:38 -0400

    Arreglos finales

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,86 +1,109 @@
-from fcntl import F_SEAL_SEAL
 import pygame
 
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, DUCKING_SHIELD, JUMPING_SHIELD, RUNNING_SHIELD, SHIELD_TYPE, RUNNING_HAMMER, JUMPING_HAMMER, DUCKING_HAMMER, HAMMER_TYPE
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import DUCKING, RUNING
-from dino_runner.utils.constants import JUMPING
+
+DUCK_IMG = {DEFAULT_TYPE: DUCKING, SHIELD_TYPE: DUCKING_SHIELD, HAMMER_TYPE: DUCKING_HAMMER}
+JUMP_IMG = {DEFAULT_TYPE: JUMPING, SHIELD_TYPE: JUMPING_SHIELD, HAMMER_TYPE: JUMPING_HAMMER}
+RUN_IMG = {DEFAULT_TYPE: RUNNING, SHIELD_TYPE: RUNNING_SHIELD, HAMMER_TYPE: RUNNING_HAMMER}
 
 
 class Dinosaur(Sprite):
     X_POS = 80
-    Y_POS = 310 
+    Y_POS = 310
     JUMP_VEL = 8.5
+    Y_POS_DUCK = Y_POS + 40
 
     def __init__(self):
-        self.image = RUNING[0]
+        self.type = DEFAULT_TYPE
+        self.image = RUN_IMG[self.type][0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
+        self.step_index = 0
         self.dino_run = True
         self.dino_jum = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
-    def update(self,user_imput):
+        self.dino_ducking_pos = self.Y_POS_DUCK
+        self.setup_state()
+
+    def setup_state(self):
+        self.has_power_up = False
+        self.shield = False
+
+        self.show_text = False
+        self.shield_time_up = 0
+
+
     def events(self):
         if self.dino_run:
             self.run()
         elif self.dino_jum:
             self.jump()
         elif self.dino_duck:
-            self.duck( )
+            self.duck()
+
     def update(self, user_imput):
         self.events()
         if user_imput[pygame.K_UP] and not self.dino_jum:
+            #sonido de salto
             self.dino_jum = True
             self.dino_run = False
             self.dino_duck = False
-        if user_imput[pygame.K_DOWN] and not self.dino_jum:
-            self.dico_duck = True
+        elif user_imput[pygame.K_DOWN] and not self.dino_jum:
+            self.dino_duck = True
+            self.dino_run = False
             self.dino_jum = False
-            self.dino_run = F_SEAL_SEAL
         elif not self.dino_jum:
             self.dino_jum = False
             self.dino_run = True
+            self.dino_duck = False
 
-        if self.step_index >=10:
-             self.step_index = 0 
-
-
-    def jump(self):
-        self.image = JUMPING
-        if self.dino_jum:
-            self.dino_rect.y -=self.jump_vel * 4
-            print(self.dino_rect.y)
-            self.jump_vel -= 0.8
-            print(self.jump_vel)
-
-        if self.jump_vel < -self.JUMP_VEL:
-            self.dino_rect.y = self.Y_POS
-            self.dino_run = False
-            self.jump_vel = self.JUMP_VEL
-        
+        if self.step_index >= 10:
+            self.step_index = 0
 
     def run(self):
-        self.image = RUNING[0] if self.step_index < 5 else RUNING[1]
+        self.image = RUN_IMG[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
         self.step_index += 1
+    
+    def jump(self):
+        self.image = JUMP_IMG[self.type]
+        if self.dino_jum:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+            
+
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_rect.y = self.Y_POS
+            self.dino_jum = False
+            self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCK_IMG[self.type][self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.dino_ducking_pos
+        self.step_index += 1
 
 
-    def draw(self, secreen: pygame.Surface):
-        self.screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+    def draw(self, screen: pygame.Surface):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def check_invincibility(self, screen):
+        if self.shield == True:
+            time_to_show = round((self.shield_time_up - pygame.time.get_ticks()) / 100, 2)
+            if time_to_show >= 0 and self.show_text:
+                # mostrar este tiempo en el juego
+
+                print(time_to_show)
+            else:
+                self.shield = False
+                self.type = DEFAULT_TYPE
+
 
 
     
-    def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else RUNING
-        if self.dino_duck:
-            self.dino_rect.y -=self.duck * 4
-            print(self.dino_rect.y)
-            self.duck_vel -= 0.8
-            print(self.duck_vel)
-
-        if self.duck_vel < -self.DUCK_VEL:
-            self.dino_rect.y = self.Y_POS
-            self.dino_run = False
-            self.duck_vel = self.DUCK_VEL
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,14 @@
 import pygame
+import random
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
+from dino_runner.utils.constants import BG, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, CLOUD
 from dino_runner.components.dinosaur import Dinosaur
+
+
 FONT_STYLE = 'freesansbold.ttf'
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -12,34 +18,47 @@ class Game:
         self.clock = pygame.time.Clock()
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
         self.playing = False
         self.running = False
-        self.game_speed = 20
+        self.game_speed = 17
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-
         self.points = 0
         self.death_count = 0
-
+        
+        self.separation = random.randint(350, 450)
+        self.x_pos_cloud1 = 0 + self.separation
+        self.x_pos_cloud2 = 0 + self.separation*2
+        self.x_pos_cloud3 = 0 + self.separation*3
+        self.x_pos_cloud4 = 0 + self.separation*4
+        self.y_pos_cloud1 = random.randint(100, 250)
+        self.y_pos_cloud2 = random.randint(100, 250)
+        self.y_pos_cloud3 = random.randint(100, 250)
+        self.y_pos_cloud4 = random.randint(100, 250)
+        self.separation = 250
+    
     def execute(self):
         self.running = True
-        while self.runing:
+        while self.running:
             if not self.playing:
-                self.show_menu():
-
+                self.show_menu()
+                
         pygame.display.quit()
         pygame.quit()
 
     def run(self):
         # Game loop: events - update - draw
         self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups(self.points, self.player)
         self.playing = True
+        self.game_speed = 20
+        self.points = 0
         while self.playing:
             self.events()
             self.update()
             self.draw()
         
-
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -47,17 +66,19 @@ class Game:
 
     def update(self):
         self.update_score()
-        user_input = pygame.key.get_pressed()
-        self.player.update(user_input)
+        given_user_input = pygame.key.get_pressed()
+        self.player.update(given_user_input)
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player )
+
     def update_score(self):
         self.points += 1
         if self.points % 100 == 0:
-           self.game_speed += 5
+            self.game_speed += 5
 
     def draw_score(self):
         font = pygame.font.Font(FONT_STYLE, 30)
-        text = font.render(f"Points: {self.points}", True, (0,0,0))
+        text = font.render(f"Point: {self.points}", True, (0, 0, 0))
         text_rect = text.get_rect()
         text_rect.center = (100,50)
         self.screen.blit(text, text_rect)
@@ -68,7 +89,9 @@ class Game:
         self.draw_background()
         self.draw_score()
         self.player.draw(self.screen)
+        self.player.check_invincibility(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -80,30 +103,60 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
-    def handle_key_events_on_menu(self):
-        for event in pygame.event.get().
-            if event.type == pygame.QUIT:
-               self.playing = False
-               self.runing = False
 
-            if event.type == pygame.KEYDOWN:
+    def handle_key_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN:
                 self.run()
 
     def show_menu(self):
-        self.screen.fill((255,255,255))
+        self.screen.fill((255, 255, 255))
         half_screen_height = SCREEN_HEIGHT // 2
         half_screen_width = SCREEN_WIDTH // 2
 
         if self.death_count == 0:
-            font = pygame.font.Font( FONT_STYLE , 30)
-            text = font.render("press any key to start", True, (0,0,0))
+            font = pygame.font.Font(FONT_STYLE, 30)
+            text = font.render("Press any key to start", True, (0, 0, 0))
             text_rect = text.get_rect()
             text_rect.center = (half_screen_width, half_screen_height)
             self.screen.blit(text, text_rect)
         elif self.death_count > 0:
-            pass
+            font = pygame.font.Font(FONT_STYLE, 50)
+            text = font.render("Game Over", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rect)
 
-        self.screen.blit(RUNNING[0], (half_screen_width - 20, half_screen_height - 140))
+
+        self.screen.blit(RUNNING[0], (half_screen_width-40, half_screen_height-140))
         pygame.display.update()
         self.handle_key_events_on_menu()
-            
+
+
+        def draw_clouds(self):
+            self.screen.blit(CLOUD, (self.x_pos_cloud1, self.y_pos_cloud1))
+            self.screen.blit(CLOUD, (self.x_pos_cloud2, self.y_pos_cloud2))
+            self.screen.blit(CLOUD, (self.x_pos_cloud3, self.y_pos_cloud3))
+            self.screen.blit(CLOUD, (self.x_pos_cloud4, self.y_pos_cloud4))
+            self.x_pos_cloud1 -= self.game_speed // 2
+            self.x_pos_cloud2 -= self.game_speed // 2
+            self.x_pos_cloud3 -= self.game_speed // 2
+            self.x_pos_cloud4 -= self.game_speed // 2
+            if self.x_pos_cloud1 <=- SCREEN_WIDTH//4:
+              self.x_pos_cloud1 = SCREEN_WIDTH
+              self.y_pos_cloud1 = random.randint(100, 250)
+            if self.x_pos_cloud2 <=- SCREEN_WIDTH//4:
+              self.x_pos_cloud2 = SCREEN_WIDTH
+              self.y_pos_cloud2 = random.randint(100, 250)
+            if self.x_pos_cloud3 <=- SCREEN_WIDTH//4:
+              self.x_pos_cloud3 = SCREEN_WIDTH
+              self.y_pos_cloud3 = random.randint(100, 250)
+            if self.x_pos_cloud4 <=- SCREEN_WIDTH//4:
+              self.x_pos_cloud4 = SCREEN_WIDTH
+              self.y_pos_cloud4 = random.randint(100, 250)
+
+
